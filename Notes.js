3990425b55f7e69386ac3c5bd918ffa757a3f399
jrslopez <lopez.jamesrussel@gmail.com>
2022-07-14 02:05:15 -0400

    Features completed

--- Notes.js
+++ Notes.js
@@ -5,6 +5,8 @@ const addNote = (title, body) => {
   const notes = loadNotes()
   const duplicateNote = notes.find((note) => note.title === title)
 
+  debugger
+
   if (!duplicateNote) {
     notes.push({
       title: title,
